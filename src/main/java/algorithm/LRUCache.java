ee7b3c67af1ebfc2693fafd8af4dbea4998a270a
s1mple <s1mple951205@gmail.com>
2022-09-04 14:46:32 +0800

    LRU算法第二版

--- src/main/java/algorithm/LRUCache.java
+++ src/main/java/algorithm/LRUCache.java
@@ -1,33 +1,43 @@
 package algorithm;
 
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.*;
 
 // least recently used: 缓存淘汰/页面置换
 public class LRUCache {
-    private HashMap<Integer, Integer> map;
-    private final LinkedList<Integer> queue;
+    private final LinkedHashMap<Integer, Integer> cache;
+    private final int capacity;
 
-    public LRUCache() {
-        queue = new LinkedList<>();
+    public LRUCache(int capacity) {
+        cache = new LinkedHashMap<>();
+        this.capacity = capacity;
     }
 
-    public void enqueue(int id) {
-        if (!queue.contains(id)) {
-            queue.addLast(id);
+    public void put(int value) {
+        if (capacity > cache.size()) {
+            cache.put(value, value);
+            visitRecently(value);
+        } else if (cache.containsKey(value)) {
+            visitRecently(value);
         } else {
-            int remove = queue.remove(queue.indexOf(id));
-            queue.addLast(remove);
+            int first = cache.keySet().iterator().next();
+            cache.remove(first);
+            cache.put(value, value);
         }
     }
 
-    public int dequeue() {
-        if (queue.size() == 0)
+    public int visit(int value) {
+        if (!cache.containsKey(value))
             return -1;
-        return queue.pollFirst();
+        visitRecently(value);
+        return cache.get(value);
     }
 
-    public LinkedList<Integer> queue() {
-        return queue;
+    private void visitRecently(int value) {
+        cache.remove(value);
+        cache.put(value, value);
+    }
+
+    public Object[] cacheSequence() {
+        return cache.keySet().toArray();
     }
 }
--- src/test/java/algorithm/LRUCacheTest.java
+++ src/test/java/algorithm/LRUCacheTest.java
@@ -4,29 +4,38 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 public class LRUCacheTest {
     @Test
-    public void should_enqueue_items_and_adjust_order() {
-        LRUCache lruCache = new LRUCache();
-        lruCache.enqueue(3);
-        lruCache.enqueue(2);
-        lruCache.enqueue(1);
-        assertThat(lruCache.queue()).containsExactly(3, 2, 1);
-        lruCache.enqueue(3);
-        assertThat(lruCache.queue()).containsExactly(2, 1, 3);
-        lruCache.enqueue(4);
-        assertThat(lruCache.queue()).containsExactly(2, 1, 3, 4);
-        lruCache.enqueue(1);
-        assertThat(lruCache.queue()).containsExactly(2, 3, 4, 1);
+    public void should_cache_values() {
+        LRUCache cache = new LRUCache(3);
+
+        cache.put(1);
+        cache.put(2);
+        assertThat(cache.cacheSequence()).containsExactly(1, 2);
+        cache.put(3);
+        assertThat(cache.cacheSequence()).containsExactly(1, 2, 3);
     }
 
     @Test
-    public void should_dequeue_lru_item() {
-        LRUCache lruCache = new LRUCache();
-        lruCache.enqueue(3);
-        lruCache.enqueue(2);
-        lruCache.enqueue(1);
-        lruCache.enqueue(3);
-        assertThat(lruCache.dequeue()).isEqualTo(2);
+    public void should_swap_out_least_recently_visit() {
+        LRUCache cache = new LRUCache(3);
+        cache.put(1);
+        cache.put(2);
+        cache.put(3);
+
+        cache.visit(1);
+        assertThat(cache.cacheSequence()).containsExactly(2, 3, 1);
+        cache.put(4);
+        assertThat(cache.cacheSequence()).containsExactly(3, 1, 4);
+        cache.visit(1);
+        assertThat(cache.cacheSequence()).containsExactly(3, 4, 1);
+    }
+
+    @Test
+    public void should_return_cache_not_hit() {
+        LRUCache cache = new LRUCache(3);
+        cache.put(1);
+        assertThat(cache.visit(2)).isEqualTo(-1);
     }
 }
