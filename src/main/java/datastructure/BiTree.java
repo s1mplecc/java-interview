8474339d4c5ad85ae4bf2d2379113268b0ebb0e7
s1mple <s1mple951205@gmail.com>
2022-09-09 18:50:38 +0800

    二叉树的前序、层次遍历

---
+++ src/main/java/datastructure/BiTree.java
@@ -0,0 +1,98 @@
+package datastructure;
+
+import java.util.*;
+
+
+public class BiTree {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * @param preOrder int整型ArrayList
+     * @param inOrder  int整型ArrayList
+     * @return TreeNode类ArrayList
+     */
+    public ArrayList<TreeNode> getBinaryTrees(ArrayList<Integer> preOrder, ArrayList<Integer> inOrder) {
+        // write code here
+        ArrayList<TreeNode> result = new ArrayList<>();
+        TreeNode root = new TreeNode(preOrder.get(0));
+        ArrayList<Integer> splitIndex = new ArrayList<>();
+        for (int i = 0; i < preOrder.size(); i++) {
+            if (inOrder.get(i) == root.val) {
+                splitIndex.add(i);
+            }
+        }
+        for (Integer index : splitIndex) {
+            ArrayList<Integer> leftTreeInOrder = new ArrayList<>();
+            for (int i = 0; i < index; i++) {
+                leftTreeInOrder.add(inOrder.get(i));
+            }
+            ArrayList<Integer> rightTreeInOrder = new ArrayList<>();
+            for (int i = index + 1; i < inOrder.size(); i++) {
+                rightTreeInOrder.add(inOrder.get(i));
+            }
+
+        }
+        return null;
+    }
+
+    private ArrayList<TreeNode> recurBinaryTrees(ArrayList<Integer> preOrder, ArrayList<Integer> inOrder, TreeNode root, ArrayList<TreeNode> result) {
+        if (preOrder.size() == 2 && Objects.equals(preOrder.get(0), inOrder.get(0))) {
+            new ArrayList<>(result);
+        }
+        return null;
+    }
+
+    /**
+     * 前序遍历
+     *
+     * @return [1, 2, 3, 4, 5]
+     */
+    public int[] preorderTraversal(TreeNode root) {
+        ArrayList<Integer> preorder = new ArrayList<>();
+        recurPreOrder(root, preorder);
+        return preorder.stream().mapToInt(Integer::valueOf).toArray();
+    }
+
+    private void recurPreOrder(TreeNode root, List<Integer> preOrder) {
+        if (root == null)
+            return;
+        preOrder.add(root.val);
+        recurPreOrder(root.left, preOrder);
+        recurPreOrder(root.right, preOrder);
+    }
+
+    /**
+     * 层序遍历
+     *
+     * @return [[1], [2, 3], [4, 5]]
+     */
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> levelOrder = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> curLevel = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                curLevel.add(node.val);
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+            levelOrder.add(curLevel);
+        }
+        return levelOrder;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
