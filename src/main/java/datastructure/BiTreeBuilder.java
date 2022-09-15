5b229b1dbd30b80530a0be7bbf7cabbd081a2030
s1mple <s1mple951205@gmail.com>
2022-09-15 21:48:07 +0800

    根据前序中序后序遍历构造二叉树

---
+++ src/main/java/datastructure/BiTreeBuilder.java
@@ -0,0 +1,84 @@
+package datastructure;
+
+import java.util.Arrays;
+
+public class BiTreeBuilder {
+    /**
+     * 题目链接：https://leetcode.cn/problems/construct-binary-tree-from-inorder-and-postorder-traversal
+     * <p>
+     * 根据一棵树的中序遍历与后序遍历构造二叉树。
+     * inorder 和 postorder 都由 不同 的值组成；postorder 中每一个值都在 inorder 中。
+     * <p>
+     * 输入：inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+     * 输出：[3,9,20,null,null,15,7]
+     */
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (postorder.length == 0)
+            return null;
+
+        int rootVal = postorder[postorder.length - 1];
+        TreeNode root = new TreeNode(rootVal);
+
+        int splitIndex = -1;
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == rootVal) {
+                splitIndex = i;
+                break;
+            }
+        }
+
+        int[] leftInorder = Arrays.copyOfRange(inorder, 0, splitIndex);
+        int[] rightInorder = Arrays.copyOfRange(inorder, splitIndex + 1, inorder.length);
+        int[] leftPostorder = Arrays.copyOfRange(postorder, 0, splitIndex);
+        int[] rightPostorder = Arrays.copyOfRange(postorder, splitIndex, inorder.length - 1);
+
+        root.left = buildTree(leftInorder, leftPostorder);
+        root.right = buildTree(rightInorder, rightPostorder);
+
+        return root;
+    }
+
+    /**
+     * 题目链接：https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal
+     * <p>
+     * 根据一棵树的前序遍历与中序遍历构造二叉树。
+     * <p>
+     * 输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+     * 输出: [3,9,20,null,null,15,7]
+     */
+    public TreeNode buildTree2(int[] preorder, int[] inorder) {
+        if (preorder.length == 0)
+            return null;
+
+        int rootVal = preorder[0];
+        TreeNode root = new TreeNode(rootVal);
+
+        int splitIndex = -1;
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == rootVal) {
+                splitIndex = i;
+                break;
+            }
+        }
+
+        int[] leftInorder = Arrays.copyOfRange(inorder, 0, splitIndex);
+        int[] rightInorder = Arrays.copyOfRange(inorder, splitIndex + 1, inorder.length);
+        int[] leftPreorder = Arrays.copyOfRange(preorder, 1, splitIndex + 1);
+        int[] rightPreorder = Arrays.copyOfRange(preorder, splitIndex + 1, preorder.length);
+
+        root.left = buildTree2(leftPreorder, leftInorder);
+        root.right = buildTree2(rightPreorder, rightInorder);
+
+        return root;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
