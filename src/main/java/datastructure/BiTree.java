699c60752538863e9f86cbe6cefa77e66138a470
s1mple <s1mple951205@gmail.com>
2022-09-15 21:54:06 +0800

    二叉树的前序、层次、之字遍历

--- src/main/java/datastructure/BiTree.java
+++ src/main/java/datastructure/BiTree.java
@@ -4,48 +4,9 @@ import java.util.*;
 
 
 public class BiTree {
-    /**
-     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
-     *
-     * @param preOrder int整型ArrayList
-     * @param inOrder  int整型ArrayList
-     * @return TreeNode类ArrayList
-     */
-    public ArrayList<TreeNode> getBinaryTrees(ArrayList<Integer> preOrder, ArrayList<Integer> inOrder) {
-        // write code here
-        ArrayList<TreeNode> result = new ArrayList<>();
-        TreeNode root = new TreeNode(preOrder.get(0));
-        ArrayList<Integer> splitIndex = new ArrayList<>();
-        for (int i = 0; i < preOrder.size(); i++) {
-            if (inOrder.get(i) == root.val) {
-                splitIndex.add(i);
-            }
-        }
-        for (Integer index : splitIndex) {
-            ArrayList<Integer> leftTreeInOrder = new ArrayList<>();
-            for (int i = 0; i < index; i++) {
-                leftTreeInOrder.add(inOrder.get(i));
-            }
-            ArrayList<Integer> rightTreeInOrder = new ArrayList<>();
-            for (int i = index + 1; i < inOrder.size(); i++) {
-                rightTreeInOrder.add(inOrder.get(i));
-            }
-
-        }
-        return null;
-    }
-
-    private ArrayList<TreeNode> recurBinaryTrees(ArrayList<Integer> preOrder, ArrayList<Integer> inOrder, TreeNode root, ArrayList<TreeNode> result) {
-        if (preOrder.size() == 2 && Objects.equals(preOrder.get(0), inOrder.get(0))) {
-            new ArrayList<>(result);
-        }
-        return null;
-    }
 
     /**
      * 前序遍历
-     *
-     * @return [1, 2, 3, 4, 5]
      */
     public int[] preorderTraversal(TreeNode root) {
         ArrayList<Integer> preorder = new ArrayList<>();
@@ -63,8 +24,6 @@ public class BiTree {
 
     /**
      * 层序遍历
-     *
-     * @return [[1], [2, 3], [4, 5]]
      */
     public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
         ArrayList<ArrayList<Integer>> levelOrder = new ArrayList<>();
@@ -90,8 +49,6 @@ public class BiTree {
 
     /**
      * 之字遍历
-     *
-     * @return [[1], [3, 2], [4, 5, 6, 7]]
      */
     public ArrayList<ArrayList<Integer>> zOrder(TreeNode root) {
         ArrayList<ArrayList<Integer>> levelOrder = new ArrayList<>();
