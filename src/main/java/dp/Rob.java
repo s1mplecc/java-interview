8661628b12c69aa4155bbd02c4466df08f37a83d
s1mple <s1mple951205@gmail.com>
2022-09-23 20:51:13 +0800

    动归解决打家劫舍1/2变型

---
+++ src/main/java/dp/Rob.java
@@ -0,0 +1,58 @@
+package dp;
+
+/**
+ * 打家劫舍
+ */
+public class Rob {
+    /**
+     * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+     * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+     * <p>
+     * 输入：[1,2,3,1]
+     * 输出：4
+     * 解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。偷窃到的最高金额 = 1 + 3 = 4 。
+     */
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0]; // dp[i] -> nums[i - 1]
+
+        for (int i = 2; i < nums.length + 1; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+
+        return dp[nums.length];
+    }
+
+    /**
+     * 这个地方所有的房屋都围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。
+     * <p>
+     * 输入：nums = [2,3,2]
+     * 输出：3
+     * 解释：你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+     */
+    public int robLoop(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        if (nums.length == 1)
+            return nums[0];
+
+        return Math.max(
+                robLoop(nums, 0, nums.length - 1),  // 不包含尾，切分为 [2, 3]
+                robLoop(nums, 1, nums.length));  // 不包含头，切分为 [3, 2]
+    }
+
+    private int robLoop(int[] nums, int start, int end) {
+        if (start == end)
+            return nums[start];
+
+        int[] dp = new int[end - start + 1];
+        dp[1] = nums[start];
+
+        for (int i = 2; i < end - start + 1; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i + start - 1]);
+        }
+
+        return dp[end - start];
+    }
+}
