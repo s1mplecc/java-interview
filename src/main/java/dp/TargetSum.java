eb0ffaa18962a97136fdb8a09013c3a52c55997b
s1mple <s1mple951205@gmail.com>
2022-09-23 18:34:21 +0800

    动归解决平方和问题

--- src/main/java/dp/TargetSum.java
+++ src/main/java/dp/TargetSum.java
@@ -1,6 +1,8 @@
 package dp;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class TargetSum {
     /**
@@ -102,11 +104,38 @@ public class TargetSum {
 
         for (int i = 1; i < amount + 1; i++) {
             for (int j = 0; j < coins.length; j++) {
-                if (i >= coins[j] && dp[i- coins[j]] != Integer.MAX_VALUE)
+                if (i >= coins[j] && dp[i - coins[j]] != Integer.MAX_VALUE)
                     dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
             }
         }
 
         return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+
+    /**
+     * 给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
+     * 给你一个整数 n ，返回和为 n 的完全平方数的 最少数量 。
+     * 完全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。例如，1、4、9 和 16 都是完全平方数，而 3 和 11 不是。
+     * <p>
+     * 示例 1： 输入：n = 12 输出：3 解释：12 = 4 + 4 + 4
+     * 示例 2： 输入：n = 13 输出：2 解释：13 = 4 + 9
+     */
+    public int numSquares(int n) {
+        List<Integer> squares = new ArrayList<>();
+        for (int i = 1; i <= (int) Math.sqrt(n); i++) {
+            squares.add(i * i);
+        }
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 0; j < squares.size() && squares.get(j) <= i; j++) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - squares.get(j)]);
+            }
+        }
+
+        return dp[n];
     }
 }
