0721a2338bcb137bdd64fed41b0f4d687f1b0259
s1mple <s1mple951205@gmail.com>
2022-09-23 22:03:22 +0800

    动归解决子序列问题（最长重复子数组）

--- src/main/java/dp/SubSequence.java
+++ src/main/java/dp/SubSequence.java
@@ -65,4 +65,27 @@ public class SubSequence {
         }
         return Arrays.stream(dp).max().getAsInt();
     }
+
+    /**
+     * 给两个整数数组 A 和 B ，返回两个数组中公共的、长度最长的子数组（连续子序列）的长度。
+     * <p>
+     * 输入：A: [1,2,3,2,1] B: [3,2,1,4,7]
+     * 输出：3
+     * 解释：长度最长的公共子数组是 [3, 2, 1] 。
+     */
+    public int findLengthOfLCCS(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        int max = 0;
+
+        for (int i = 1; i < nums1.length + 1; i++) {
+            for (int j = 1; j < nums2.length + 1; j++) {
+                if (nums1[i - 1] == nums2[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+
+        return max;
+    }
 }
--- src/test/java/dp/SubSequenceTest.java
+++ src/test/java/dp/SubSequenceTest.java
@@ -12,4 +12,17 @@ public class SubSequenceTest {
         assertThat(sequence.lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18})).isEqualTo(4);
         assertThat(sequence.lengthOfLIS(new int[]{1, 3, 6, 7, 9, 4, 10, 5, 6})).isEqualTo(6);
     }
+
+    @Test
+    public void should_find_length_of_lccs() {
+        SubSequence sequence = new SubSequence();
+
+        assertThat(sequence.findLengthOfLCCS(
+                new int[]{0, 1, 1, 1, 1},
+                new int[]{1, 0, 1, 0, 1})).isEqualTo(2);
+
+        assertThat(sequence.findLengthOfLCCS(
+                new int[]{1, 2, 3, 2, 1},
+                new int[]{3, 2, 1, 4, 7})).isEqualTo(3);
+    }
 }
