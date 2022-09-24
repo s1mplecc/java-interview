2fe24f32fbc9c422849978034d858a17523432cd
s1mple <s1mple951205@gmail.com>
2022-09-24 11:42:59 +0800

    动归解决子序列问题（最大子数组和）

--- src/main/java/dp/SubSequence.java
+++ src/main/java/dp/SubSequence.java
@@ -79,11 +79,34 @@ public class SubSequence {
 
         for (int i = 1; i < nums1.length + 1; i++) {
             for (int j = 1; j < nums2.length + 1; j++) {
-                if (nums1[i - 1] == nums2[j - 1]){
+                if (nums1[i - 1] == nums2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                     max = Math.max(max, dp[i][j]);
                 }
             }
+        }
+
+        return max;
+    }
+
+    /**
+     * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+     * <p>
+     * 输入: [-2,1,-3,4,-1,2,1,-5,4]
+     * 输出: 6
+     * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+     */
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];  // dp[i]表示以nums[i]结尾最大连续子数组和
+        dp[0] = nums[0];
+        int max = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (dp[i - 1] <= 0)
+                dp[i] = nums[i];
+            else
+                dp[i] = dp[i - 1] + nums[i];
+            max = Math.max(dp[i], max);
         }
 
         return max;
